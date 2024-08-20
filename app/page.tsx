0faa4f2217c6cdc29b55190c29879995e947da81
amitbright <amit@brightroots.in>
2024-08-20 13:50:16 +0530

    fix issue

--- app/page.tsx
+++ app/page.tsx
@@ -84,8 +84,78 @@ export default function Home() {
   const table = useMantineReactTable<TemplateItem>({
     columns: columns,
     data: templateData,
-    ...tableConfig
-   
+    enableColumnOrdering: true,
+    enableDensityToggle: false,
+    enableStickyHeader: true,
+    enableStickyFooter: true,
+    enableRowSelection: true,
+    enableColumnResizing: true,
+    enableClickToCopy: false,
+    enableColumnDragging: false,
+    enableColumnPinning: true,
+    layoutMode: "grid",
+    positionActionsColumn: "last",
+    mantineSelectAllCheckboxProps: { size: "sm" },
+    getRowId: (originalRow) => originalRow.id,
+    initialState: {
+      pagination: { pageSize: 20, pageIndex: 0 },
+    },
+    mantineSelectCheckboxProps: {
+      className: "h-[20px] w-[20px] !p-0",
+      size: "sm",
+    },
+    mantineFilterTextInputProps: {
+      className: "border-b-0 mt-0 p-0 bg-transparent",
+    },
+    mantineFilterSelectProps: {
+      className: "border-b-0 mt-0 p-0 bg-transparent",
+    },
+    mantineFilterMultiSelectProps: {
+      className: "border-b-0 mt-0 p-0 bg-transparent",
+    },
+    mantineTableBodyRowProps: (props) => ({
+      classNames: {
+        tr: props.row.getIsSelected()
+          ? "!bg-brand-50"
+          : "bg-inherit cursor-pointer",
+      },
+    }),
+    defaultColumn: {
+      mantineTableBodyCellProps: {
+        classNames: () => {
+          return {
+            td: "border-0 border-r-[1px] last:border-r-0 border-divider border-solid first:px-[20px] px-[12px] py-[10px]",
+          };
+        },
+      },
+      Cell: (props) => (
+        <Text className="text-body-14 text-primary" lineClamp={2}>
+          {props.renderedCellValue}
+        </Text>
+      ),
+    },
+    mantinePaperProps: {
+      className: "border-r rounded-2xl border-divider border-solid",
+      style: {
+        "--mrt-row-hover-background-color": "rgb(var(--color-brand-50))",
+      },
+    },
+    mantineTableContainerProps: {
+      className: "rounded-[8px] max-h-[70vh]",
+    },
+    mantineTableHeadCellProps: {
+      classNames: {
+        th: "bg-primary text-secondary text-caps-10 uppercase first:px-[20px] pl-[12px] py-[5px] min-h-[32px]",
+      },
+      style: {
+        backgroundColor: "rgb(var(--color-neutrals-100))",
+      },
+    },
+    displayColumnDefOptions: {
+      "mrt-row-actions": {
+        minSize: 130,
+      },
+    },
   });
 
   return (
@@ -94,5 +164,3 @@ export default function Home() {
     </main>
   );
 }
-
-
